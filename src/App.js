576af45010002e009c756824cdbeab9a3781581d
Violet <94542413+chenhebin@users.noreply.github.com>
2025-01-31 22:46:20 +0800

    feat: react project init

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import './index.css'
+function App() {
+    return (
+        <div className="App">
+            <br/>Hello World<br/>
+        </div>
+    );
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'violetBundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+            // 添加 CSS 文件的处理规则
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './public/index.html'
+        })
+    ],
+    devServer: {
+        port: 3001,
+    }
+};
